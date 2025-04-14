2deac70214bc8d4654266499ad1184ab47fe32df
obadea <obadeagbenga@gmail.com>
2025-04-14 16:19:39 +0100

    Initial commit

---
+++ app/(app)/page.tsx
@@ -0,0 +1,7 @@
+export default function SinkPage() {
+  return (
+    <div className="@container grid flex-1 gap-4 p-4">
+      <div className="text-3xl font-bold text-red-400">helo</div>
+    </div>
+  )
+}
